34def256a9af7727d45c1514f02f35701e50af37
Henry Moule <hen10@hotmail.co.uk>
2023-03-04 22:19:50 +0000

    init firebase

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world")
+import * as admin from 'firebase-admin';
+import sgMail from '@sendgrid/mail'
+import moment from 'moment';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://health-and-movement.firebaseio.com"
+});
+
+const db = admin.firestore();
+const storage = admin.storage();
+
+
+
